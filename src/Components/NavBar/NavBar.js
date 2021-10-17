dd6b43aa6f74c3f95f2dce62ff3b162ac828adb4
Jake Hracho <jhracho@nd.edu>
2021-10-17 18:14:29 -0400

    Routing Complete

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,20 @@
 // navbar component displayed at top of page to link to other pages
 // NOTE: links will be changed as more pages are converted to js instead of static html
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
     <div class='navbar'>
         <ul class='left'>
-            <li class="active">Home</li>
-            <li><a href='#'>About</a></li>
-            <li><a href=''>Explore</a></li>
-            <li><a href=''>Post</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to='/explore'>Explore</Link></li>
+            <li><Link to='/create'>Post</Link></li>
         </ul>
         <ul class='right'>
-            <li><a href='#'>Log In</a></li>
-            <li><a href='#'>Sign Up</a></li>
+            <li><Link to="/login">Log In</Link></li>
+            <li><Link to='/signup'>Sign Up</Link></li>
             <li>Log Out</li>
         </ul>
     </div>
