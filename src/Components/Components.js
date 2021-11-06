1a877165bc646459e63d56171c29efd3b727a0d7
ntucker2 <ntucker2@nd.edu>
2021-11-05 23:05:12 -0400

    switched components to protected pages

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,7 +2,7 @@ import React from "react";
 import HomePage from "./Home/HomePage.js";
 import AboutPage from "./About/AboutPage.js";
 import ExplorePage from "./Explore/ExplorePage.js";
-import CreatePage from "./Create/CreatePage.js";
+import CreateProtected from "./Create/CreateProtected.js";
 import LogInPage from "./Log In/LogInPage.js";
 import SignUpPage from "./Sign Up/SignUpPage.js";
 
@@ -23,7 +23,7 @@ const Components = () => {
          <Route path="/" exact component={HomePage} />
          <Route path="/about" component={AboutPage} />
          <Route path="/explore" component={ExplorePage} />
-         <Route path="/create" component={CreatePage} />
+         <Route path="/create" component={CreateProtected} />
          <Route path="/login" component={LogInPage} />
          <Route path="/signup" component={SignUpPage} />
          <Redirect to="/" />
