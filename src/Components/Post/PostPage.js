679f7a8e8a2b08d6ae738035ecc3f6b1ea00adb8
Jake Hracho <jhracho@nd.edu>
2021-11-24 21:33:24 -0500

    Post pages now display text in <p>s

--- src/Components/Post/PostPage.js
+++ src/Components/Post/PostPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getPostById } from "../../Services/PostService.js";
 
-import Heading from "./Heading.js";
+import Story from "./Story.js";
 
 const PostPage = () =>{
     const params = useParams();
@@ -14,29 +14,11 @@ const PostPage = () =>{
             setPost(response);
           });
     }, []);
-        
+
+    // Leading Indent or &nbsp; or \n\n
     return(
         <div class='post-box'>
-            {post && <Heading post={post}/>} 
-
-            <div class="line"></div>
-
-            <div class='post'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac tempor neque. Nulla sit 
-                    amet tellus id massa elementum pellentesque in a arcu. Donec eget lacus pharetra, vestibulum metus ut, 
-                    dapibus velit. Sed ac urna pellentesque, fermentum orci vel, bibendum magna. Morbi ac quam quam. Sed 
-                    aliquet elit sit amet mauris iaculis, nec fermentum urna maximus. Donec ligula ante, sagittis eu lacus ac, 
-                    aliquet efficitur magna.</p>
-                <p>Phasellus in turpis quis nibh condimentum pulvinar. Praesent malesuada enim erat, eu mollis ipsum ornare eu. 
-                    Aenean vestibulum hendrerit nulla, fringilla elementum est finibus hendrerit. Suspendisse potenti. Phasellus 
-                    et finibus eros, non volutpat massa. In vel maximus nibh. Class aptent taciti sociosqu ad litora torquent per 
-                    conubia nostra, per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, 
-                    nascetur ridiculus mus. Phasellus eget est sollicitudin lacus tempor vestibulum sed non eros. Suspendisse vel 
-                    volutpat urna. Nunc ut consequat neque. Sed nibh lectus, posuere ac consectetur non, interdum non sem. Phasellus 
-                    consequat vehicula arcu at condimentum. Nam at lacus vitae metus ornare sollicitudin et nec libero. Nam tincidunt 
-                    venenatis fringilla.</p>
-            </div>
-            
+            {post && <Story post={post}/>} 
         </div>
     );
 };
---
+++ src/Components/Post/Story.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { getAuthorById } from "../../Services/AuthorService.js";
+
+const Heading = (post) =>{
+    // Get author display name from the author attribute of post object
+    const [author, setAuthor] = useState(null);
+    useEffect(() => {
+        getAuthorById(post.post.get("author").id).then((response) => {
+            setAuthor(response);
+          });
+    }, []);
+
+    // Manipulate the createdAt property to make it user-readable
+    const createdAt = post.post.createdAt.toString().split(" ").slice(0,5).join(" ");
+
+    // Create paragraph array and remove any extra newlines
+    const arr = post.post.get('text').split('\n');
+    var i = 0;
+    while (i < arr.length) {
+        if (arr[i] == '') {
+            arr.splice(i, 1);
+        } 
+        else {
+            i++;
+        }
+    }
+
+    return(
+        <Fragment>
+            <div class='post-heading'>
+                <h1>{post.post.get("title")}</h1>
+                <h2><em>{post.post.get("subtitle")}</em></h2>
+                <h3>{author && author.get('displayname')} | {createdAt}</h3>
+
+                <i id='post-like' class="fas fa-thumbs-up">{post.post.get("likes")}</i>
+                <i id='post-dislike' class="fas fa-thumbs-down">{post.post.get("dislikes")}</i>
+            </div>
+            
+            <div class='line'></div>
+
+            {arr.map((paragraph) => (
+                <p>{paragraph}</p>
+            ))}
+        </Fragment>
+    );
+};
+
+export default Heading;
